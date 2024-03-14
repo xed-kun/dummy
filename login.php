cd4eb9eeedbe61423027cded296b7a60ca5ae839
xed-kun <xedricllonel12@gmail.com>
2024-03-15 03:02:35 +0800

    XXXX

---
+++ login.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "covermymeds";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//Check connection 
+if ($conn->connect_error){
+  die("Connection has failed." . $conn->connect_error);
+}
+
+// Check if form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  // Check if the login attempt counter exists in the session
+  if (!isset($_SESSION['login_attempts'])) {
+    $_SESSION['login_attempts'] = 0;
+  }
+
+  // Check if the user has reached the maximum login attempts
+  if ($_SESSION['login_attempts'] >= 5) {
+    echo "<script>alert('You have reached the maximum number of login attempts. Please try again later.');</script>";
+  } else {
+    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = $conn->query($sql);
+
+    // Check if match
+    if ($result->num_rows > 0) {
+      echo "Login Success!";
+      header("Location: ./request/index.html");
+    } else {
+      $_SESSION['login_attempts']++; 
+      echo "<script>alert('Invalid username or password');</script>";
+    }
+  }
+}
+
+$conn->close();
+?>
---
+++ signup.php
@@ -0,0 +1,63 @@
+<?php
+//database connection 
+$host = "localhost";
+$dbusername = "root";
+$dbpass = "";
+$dbname = "covermymeds";
+
+$conn = mysqli_connect($host, $dbusername, $dbpass, $dbname);
+
+//checking the connection 
+
+if ($conn->connect_error){
+  die("Connection has failed: " . $conn->connect_error);
+
+}
+
+//checking if form is submitted
+if (isset($_POST['submit'])){
+  //RETRIVE FORM
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $cpass = $_POST['cpass']; 
+  $email = $_POST["Email"];
+
+
+$sql="select * from users where username='$username'";
+$result = mysqli_query($conn, $sql);
+$count_user = mysqli_num_rows($result);
+
+$sql="select * from users where email='$email'";
+$result = mysqli_query($conn, $sql);
+$count_email = mysqli_num_rows($result);
+
+//searching if there is identical names/emails, also hashing the password for security
+if($count_user == 0 && $count_email == 0){  
+  if($password==$cpass){
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+    $sql = "INSERT INTO users (username, password, email) VALUES ('$username', '$hash' , '$email')";
+    $result = mysqli_query($conn, $sql);
+    if($result){
+      // Redirect to login page after successful sign-up
+      header("Location: ./login.html");
+      exit();
+    }
+  }
+  else{
+    echo 'Passwords do not match' ;
+  }
+}
+  else{
+    if($count_user>0){
+      echo 'This user already exists!';
+    }
+    if($count_email>0){
+      echo 'This email already exists!';
+    }
+    else
+     echo 'User must be filled';
+  }
+
+
+}
+?>
